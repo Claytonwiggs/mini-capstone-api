d587dbc7e0324711429e524cd88dc892af3b8c0a
claytonwiggs <claytonwiggs@gmail.com>
2021-05-04 20:34:18 -0400

    displaying all the products, adding routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  def product_info
+    products = Product.all
+    render json: { message: products }
+  end
+
+  def product1_info
+    product = Product.find(1)
+    render json: { message: product }
+  end
+
+  def product2_info
+    product = Product.find(2)
+    render json: { message: product }
+  end
+
+  def product3_infor
+    product = Product.find(3)
+    render json: { message: product }
+  end
 end
