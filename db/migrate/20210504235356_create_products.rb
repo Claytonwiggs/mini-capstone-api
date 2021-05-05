7acbd6f2f6739bef5813f6d4b57c49fcd0ec7dd8
claytonwiggs <claytonwiggs@gmail.com>
2021-05-04 20:12:22 -0400

    Seeds Add

--- db/migrate/20210504235356_create_products.rb
+++ db/migrate/20210504235356_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.Integer :price
+      t.integer :price
       t.string :image_url
       t.string :description
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Apple", price: 10, image_url: " ", description: "Round and Red")
+Product.create(name: "Orange", price: 8, image_url: " ", description: "Round and Orange")
+Product.create(name: "Grapes", price: 9, image_url: " ", description: "Round and purple, also lots of them")
