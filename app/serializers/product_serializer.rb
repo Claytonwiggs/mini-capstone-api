9a4ce250746751bd668ad422303abe1883720ca2
claytonwiggs <claytonwiggs@gmail.com>
2021-05-11 22:27:55 -0400

    Fixing Image Table

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :image_url, :description, :supplier
+  attributes :id, :name, :price, :image_url, :description, :supplier, :image
 end
