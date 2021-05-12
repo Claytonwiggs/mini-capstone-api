75ca4d00a526b89fbe6ebf450b7e9aafe156ff1d
claytonwiggs <claytonwiggs@gmail.com>
2021-05-11 21:26:43 -0400

    Supplier Model, Controllers, and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
+  get "/suppliers" => "suppliers#index"
+  post "/supplier" => "suppliers#create"
+  get "/supplier/:id" => "suppliers#show"
+  patch "/supplier/:id" => "suppliers#update"
+  delete "/supplier/:id" => "suppliers#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 Product.create(name: "Apple", price: 10, image_url: "https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&ssl=1", description: "Round and Red")
 Product.create(name: "Orange", price: 8, image_url: "https://www.telegraph.co.uk/multimedia/archive/01834/orange_1834038b.jpg", description: "Round and Orange")
 Product.create(name: "Grapes", price: 9, image_url: "https://images.albertsons-media.com/is/image/ABS/184100012?$ecom-pdp-desktop$&defaultImage=Not_Available&defaultImage=Not_Available", description: "Round and purple, also lots of them")
+Supplier.create(name: "Kroger", email: "kroger@gmail.com", phone_number: "123-456-7896")
+Supplier.create(name: "Walmart", email: "walmart@gmail.com", phone_number: "121-446-7896")
